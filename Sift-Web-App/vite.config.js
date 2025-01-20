596f534e240a24e359758421f026f4cb65fa5650
jtgraham04 <jtg021@uark.edu>
2025-01-20 09:09:36 -0600

    Initial commit for deployment

--- Sift-Web-App/vite.config.js
+++ Sift-Web-App/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sift-web-app/',
+  base: '/Sift-web-app',
   server: {
     proxy: {
       '/api': {
