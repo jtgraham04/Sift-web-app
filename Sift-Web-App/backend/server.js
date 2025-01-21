1ed69d754894212124e7eb55b55310426022af49
jtgraham04 <jtg021@uark.edu>
2025-01-21 11:49:09 -0600

    sdg

--- Sift-Web-App/backend/server.js
+++ Sift-Web-App/backend/server.js
@@ -33,6 +33,6 @@ app.use('/api/questionnaire', questionnaireRoutes);
 
 
 
-app.listen(PORT, '0.0.0.0', function() {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5002, '0.0.0.0', function() {
+  console.log('Listening to port:  ' + 5002);
 });
